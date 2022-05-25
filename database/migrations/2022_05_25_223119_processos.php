b66a4d70e0c890adc502569017846d4cc23c8eeb
Vinicius Costa <viniciusath@hotmail.com>
2022-05-25 20:16:55 -0300

    first commit

---
+++ database/migrations/2022_05_25_223119_processos.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Processos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('processos', function (Blueprint $table) {
+            $table->id('ProcessoID');
+            $table->integer('ClientCodigo');
+            $table->integer('DespachanteID');
+            $table->integer('TerminalAtracacaoID');
+            $table->integer('NumeroProcesso');
+            $table->integer('NumeroDI');
+            $table->date('DataDI');
+            $table->date('DataLI');
+            $table->date('DataDesembaraco');
+            $table->date('DataEntrega');
+            $table->date('DataFechamento');
+            $table->boolean('Ativo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('processos');
+    }
+}
