c16f89238874d7e604111dde8a2f28b1fefb1465
Vinicius Costa <viniciusath@hotmail.com>
2022-05-27 09:30:44 -0300

    Details

--- resources/views/processos.blade.php
+++ resources/views/processos.blade.php
@@ -25,7 +25,7 @@
         }
     </style>
 </head>
-<body class="antialiased">cd 
+<body class="antialiased">
 <div
     class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
     <div class="container">
